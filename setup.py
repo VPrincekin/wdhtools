d9610809f30cd3cbbdf87f981c8febac649b644f
fotoable <fotoable@fotoables-MacBook-Pro-2.local>
2020-07-27 17:46:40 +0800

    update

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='wdhtools',
-    version='1.1.1',
+    version='1.1.2',
     author='wdh',
     packages=find_packages(), install_requires=['pandas', 'sklearn', 'xgboost', 'matplotlib', 'seaborn', 'featexp']
 )
